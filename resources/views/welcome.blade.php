847d5e274f897b65ca1e0bcc79b1f50d59f90d00
Yessica <yessicachoquemamani97@gmail.com>
2023-09-07 00:19:49 -0400

    feat: firtst commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,6 @@
 @section('title','Proyecto INF 281')
 @section('content')
     <h1>"Bienvenido a la página principal del proyecto"</h1>
+    <he>YESSICA</H2>
     <span class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">INF 281</span>
 @endsection
